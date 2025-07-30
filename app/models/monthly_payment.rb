6870e8a9feb32422e438083ac01f5cd9639b3163
sarah <sar.brisson@gmail.com>
2025-07-30 10:48:48 +0200

    Round all results

--- app/models/monthly_payment.rb
+++ app/models/monthly_payment.rb
@@ -24,7 +24,7 @@ class MonthlyPayment
     @end_date = end_date
     @salary = salary.to_f 
     @last_period_leave_value = last_period_leave_value.to_f
-    @payment_by_ten_percent_rest = payment_by_ten_percent_rest.to_f
+    @payment_by_ten_percent_rest = payment_by_ten_percent_rest.round(2)
     @payment_by_the_dozen_rest = 0.0
     @payment_by_ten_percent = 0.0
     @current_period_leave_value = 0.0
@@ -47,7 +47,7 @@ class MonthlyPayment
 
   def set_payment_in_june
     if @start_date.month == 6
-      @payment_in_june = @last_period_leave_value
+      @payment_in_june = @last_period_leave_value.round(2)
     else
       @payment_in_june = 0.0
     end
@@ -62,7 +62,7 @@ class MonthlyPayment
   end
 
   def set_payment_by_ten_percent
-    @payment_by_ten_percent = @perceived_salary * 0.1
+    @payment_by_ten_percent = (@perceived_salary * 0.1).round(2)
     # If it's June, we need to add the rest of the last period
     if @start_date.month == 6
       @payment_by_ten_percent += @payment_by_ten_percent_rest
@@ -72,14 +72,14 @@ class MonthlyPayment
   def adjust_payments_end_of_contract
     # Payment in June
     # If it's june we need to pay the last period leave value + this period's value
-    @payment_in_june = @payment_in_june + @current_period_leave_value
+    @payment_in_june = (@payment_in_june + @current_period_leave_value).round(2)
 
     # Payment by the dozen
     # We need to pay the rest that is due + the accumulation from the current period
-    @payment_by_the_dozen = @payment_by_the_dozen + @payment_by_the_dozen_rest + @current_period_leave_value
+    @payment_by_the_dozen = (@payment_by_the_dozen + @payment_by_the_dozen_rest + @current_period_leave_value).round(2)
 
     # Payment by ten percent
     # We need only need to pay the rest that is due
-    @payment_by_ten_percent = @payment_by_ten_percent + @payment_by_ten_percent_rest
+    @payment_by_ten_percent = (@payment_by_ten_percent + @payment_by_ten_percent_rest).round(2)
   end
 end
